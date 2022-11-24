551341bf9adbfefbb41920334d6c3ff192176bf2
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2022-11-24 16:52:47 -0600

    Educational Codeforces Round 100 (Rated for Div. 2)

---
+++ codeforces/Educational Codeforces Round 100 (Rated for Div. 2)/A.cpp
@@ -0,0 +1,22 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+* LinkedIn: https://www.linkedin.com/in/jorgetzab/
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if((a + b + c) % 9 || min({a, b, c}) < (a + b + c) / 9) cout << "NO\n";
+        else cout << "YES\n";
+    }
+}
