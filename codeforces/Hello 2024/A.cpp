d6376a34f2a1d2ee3794b69d6008fe92038da9e1
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2024-01-06 13:41:13 -0600

    Concurso codeforces

---
+++ codeforces/Hello 2024/A.cpp
@@ -0,0 +1,20 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    while(t--){
+        int a, b; cin >> a >> b;
+        if((a + b) % 2) cout << "Alice\n";
+        else cout << "Bob\n";
+    }
+}
---
+++ codeforces/Hello 2024/B.cpp
@@ -0,0 +1,22 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        string str; cin >> str;
+        int ans = 0;
+        for(char c : str) ans += c == '+' ? 1 : -1;
+        cout << abs(ans) << '\n';
+    }
+}
