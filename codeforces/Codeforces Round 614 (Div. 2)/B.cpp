ea4a93c1608b14a140627c0a146d36fde09604dc
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2025-06-13 10:15:23 -0600

    Practica combi
    1000?

---
+++ codeforces/Codeforces Round 614 (Div. 2)/B.cpp
@@ -0,0 +1,22 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n; cin >> n;
+    long double ans = 0;
+    while(n) ans += 1.0 / n--;
+    cout << fixed << setprecision(5) << ans << '\n';
+}
