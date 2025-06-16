2e05dc397e434fc0529f6b375f71f87279f42420
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2025-06-15 19:42:24 -0600

    Create A.cpp

---
+++ codeforces/Codeforces Round 1014 (Div. 2)/A.cpp
@@ -0,0 +1,40 @@
+#/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+const ll MOD = 1e9 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        int arr[n], ans = 0;
+        for(int i = 0; i < n; ++i){
+            cin >> arr[i];
+            for(int j = 0; j < i; ++j){
+                ans = max(ans, abs(arr[i] - arr[j]));
+            }
+        }
+        cout << ans << '\n';
+    }
+}
