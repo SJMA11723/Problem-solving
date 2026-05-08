c48e0b97f9020795b2409a163ecdeaf86df4312b
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2026-05-08 17:24:52 -0600

    Upsolving ICPC

--- ICPC/LATAM/2019/2019-2020 ACM-ICPC Latin American Regional Programming Contest/F.cpp
+++ ICPC/LATAM/2019/2019-2020 ACM-ICPC Latin American Regional Programming Contest/F.cpp
@@ -1,39 +1,54 @@
-#include<bits/stdc++.h>
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const int MOD = 1e9 + 7;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)(x).size()
+
+const ll MOD = 1e9 + 7;
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int S, n; cin >> S >> n;
+    int S, B; cin >> S >> B;
 
-    int dp[n + 1][S + 1];
-    memset(dp, 0, sizeof(dp));
-    dp[0][0] = 1;
-    for(int i = 1; i <= n; ++i) dp[i][1] = 1;
-
-    for(int i = 2; i <= S; ++i){
-        for(int b = 0; b <= n; ++b){
-            for(int M = 0; M <= b; M += i){
-                dp[b][i] += dp[b - M][i - 1];
-                if(dp[b][i] >= MOD) dp[b][i] -= MOD;
+    B -= S;
+    int dp[S + 1][B + 1];
+    for(int s = 0; s <= S; ++s){
+        for(int b = 0; b <= B; ++b){
+            dp[s][b] = 0;
+            if(!b){
+                dp[s][b] = 1;
+                continue;
             }
 
-            cout << dp[b][i] << ' ';
-        }
-        cout << '\n';
-    }
+            if(s && s <= b) dp[s][b] += dp[s][b - s];
+            if(dp[s][b] >= MOD) dp[s][b] -= MOD;
+            
+            if(1 <= s) dp[s][b] += 2 * dp[s - 1][b] % MOD;
+            if(dp[s][b] >= MOD) dp[s][b] -= MOD;
 
-
-    int ans = 0;
-    for(int i = 0; i <= S; ++i){
-        for(int j = 0; j <= n; ++j){
-            ans += 1ll * dp[j - i][i] * dp[n - j - S + i][S - i] % MOD;
-            if(ans >= MOD) ans -= MOD;
+            if(2 <= s) dp[s][b] += (MOD - dp[s - 2][b]) % MOD;
+            if(dp[s][b] >= MOD) dp[s][b] -= MOD;
         }
     }
-    cout << ans << '\n';
+
+    cout << dp[S][B] << '\n';
 }
