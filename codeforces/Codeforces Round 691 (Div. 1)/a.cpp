6314434e07b626a70fb5f5f45e8fe25d27298139
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2026-07-04 01:05:55 -0600

    Practica N

---
+++ codeforces/Codeforces Round 691 (Div. 1)/a.cpp
@@ -0,0 +1,43 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)(x).size()
+
+const int MOD = 1e9 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m; cin >> n >> m;
+    ll a; cin >> a;
+    ll g = 0;
+    for(int i = 1; i < n; ++i){
+        ll x; cin >> x;
+        g = gcd(g, x - a);
+    }
+    for(int i = 0; i < m; ++i){
+        ll b; cin >> b;
+        cout << gcd(g, a + b) << " \n"[i + 1 == m];
+    }
+}
